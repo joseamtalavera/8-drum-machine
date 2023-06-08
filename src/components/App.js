b7f6b6bc99a892ee3ad7320fa49a301be8438cbf
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2023-06-08 18:10:41 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import Pad from './Pad';
+
+const audioClips = [
+  { id: "Heater-1", letter: "Q", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
+  { id: "Heater-2", letter: "W", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
+  { id: "Heater-3", letter: "E", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
+  { id: "Heater-4", letter: "A", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" },
+  { id: "Clap", letter: "S", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
+  { id: "Open-HH", letter: "D", src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
+  { id: "Kick-n-Hat", letter: "Z", src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" },
+  { id: "Kick", letter: "X", src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
+  { id: "Closed-HH", letter: "C", src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" }
+];
+
+const App = () => {
+  const [display, setDisplay] = useState('Display');
+
+
+  const handleDisplay = (display) => setDisplay(display);
+
+
+
+  const handleKeyDown = (e) => {
+    const key = e.key.toUpperCase();
+    const clip = audioClips.find(c => c.letter === key);
+    if (clip) {
+      const audio = document.getElementById(key);
+      audio.currentTime = 0;
+      audio.play();
+      handleDisplay(clip.id);
+    }
+  };
+
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+
+  }, []);
+
+
+
+
+
+  return (
+    <div id="drum-machine" className="App">
+      <div id="display">{display}</div>
+      {audioClips.map (clip => (
+      <Pad 
+      key={clip.id} 
+      id={clip.id} 
+      letter={clip.letter}
+      src={clip.src} 
+      handleDisplay={handleDisplay} 
+      />
+      ))}
+    </div>
+  );
+};
+
